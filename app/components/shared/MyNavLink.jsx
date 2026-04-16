fc5a099bd51b5318bdcb82fdb425f262b0135b94
Md. Nihal Uddin Bijoy <mdnihaluddinbijoy@gmail.com>
2026-04-16 11:51:10 +0600

    Navigation active issue for app details fixed

--- app/components/shared/MyNavLink.jsx
+++ app/components/shared/MyNavLink.jsx
@@ -3,7 +3,7 @@ import { usePathname } from 'next/navigation';
 
 const MyNavLink = ({to, children}) => {
     const pathName = usePathname()
-    const isActive = pathName === to
+    const isActive = pathName === to || pathName.startsWith(to + '/')
     return (
         <Link href={to} className={ `pb-1 ${isActive? "text-purple-600 border-b border-purple-600 ": ""}`}>{children}</Link>
     );
